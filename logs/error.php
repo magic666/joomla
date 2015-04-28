2d28b4b9b58ec43d239c9becd125dd607ef204be
Maciek Magic <whitewolf13@wp.pl>
2015-04-28 13:32:16 +0200

    Lekko zrobiony

---
+++ logs/error.php
@@ -0,0 +1,9 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2015-04-27 11:38:39 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2015-04-27T11:38:39+00:00	INFO 127.0.0.1	joomlafailure	Username and password do not match or you do not have an account yet.
+2015-04-27T11:38:42+00:00	INFO 127.0.0.1	joomlafailure	Empty password not allowed.
+2015-04-27T11:38:52+00:00	INFO 127.0.0.1	joomlafailure	Empty password not allowed.
